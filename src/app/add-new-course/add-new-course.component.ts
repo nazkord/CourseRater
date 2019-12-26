56e0f5d00c1a4a15beda72767d5238ced1369a0e
nazkord <nazkord@gmail.com>
2019-12-26 02:10:08 +0100

    added validation logic for course form

--- src/app/add-new-course/add-new-course.component.ts
+++ src/app/add-new-course/add-new-course.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, Form } from '@angular/forms';
 import { Course, ClassForm } from '../model/course';
 
 @Component({
@@ -9,52 +9,92 @@ import { Course, ClassForm } from '../model/course';
 })
 export class AddNewCourseComponent implements OnInit {
 
-  newCourse: Course = {
-    id: null,
-    courseName: null,
-    ects: null,
-    semester: null,
-    courseForm: null,
-    maxNumberOfStudent: null,
-    rate: null,
-    logoPath: null,
-    description: null
-  };
+  newCourse: Course;
   courseTypes = Object.values(ClassForm).slice(0,4);
   formOfCourse: FormGroup;
+  courseName: FormControl;
+  ects: FormControl;
+  semester: FormControl;
+  maxNumberOfStudent: FormControl;
+  courseForm: FormControl;
+  description: FormControl;
+  logoPath: FormControl; 
 
   constructor() { }
 
   ngOnInit() {
+    this.emptyCourse();
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+      this.courseName = new FormControl(
+        '', [
+          Validators.required,
+          
+        ]
+      ),
+      this.ects = new FormControl(
+        '', [
+          Validators.required
+        ]
+      ),
+      this.semester = new FormControl(
+        '', [
+          Validators.required,
+          Validators.min(0),
+          Validators.max(15)
+        ]
+      ),
+      this.maxNumberOfStudent = new FormControl(
+        '', [
+          Validators.required,
+          Validators.min(0)
+        ]
+      ),
+      this.courseForm = new FormControl(
+        '', [
+          Validators.required
+        ]
+      ),
+      this.description = new FormControl(
+        '', [
+          Validators.required
+        ]
+      ),
+      this.logoPath = new FormControl(
+        '', [
+          Validators.required,
+          Validators.pattern("https://.+\.[jpg|gif|png].*")
+        ]
+      )
+  }
+
+  createForm() {
     this.formOfCourse = new FormGroup({
-        courseName: new FormControl(
-          '', [
-            Validators.required,
-            
-          ]
-        ),
-        ects: new FormControl(
-
-        ),
-        semester: new FormControl(
-
-        ),
-        maxNumberOfStudent: new FormControl(
-
-        ),
-        courseForm: new FormControl(
-
-        ),
-        description: new FormControl(
-
-        ),
-        logoPath: new FormControl(
-          '', [
-            Validators.required,
-            Validators.pattern("https://.+\.[jpg|gif|png].*")
-          ]
-        )
+      courseName: this.courseName,
+      ects: this.ects,
+      semester: this.semester,
+      maxNumberOfStudent: this.maxNumberOfStudent,
+      courseForm: this.courseForm,
+      description: this.description,
+      logoPath: this.logoPath
     });
+  }
+
+  emptyCourse() {
+    this.newCourse = {
+      id: null,
+      courseName: null,
+      ects: null,
+      semester: null,
+      courseForm: null,
+      maxNumberOfStudent: null,
+      rate: null,
+      logoPath: null,
+      description: null
+    }
   }
 
 }
